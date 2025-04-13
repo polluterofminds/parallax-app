375254a107baf6fa852b68a1c9496ebca85214fe
Justin Hunter <justin@pinata.cloud>
2025-04-13 07:59:44 -0500

    adding logging

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -39,7 +39,10 @@ function Welcome() {
     };
 
     if (address) {
+      alert("Address found, getting deposit status")
       getDepositStatus();
+    } else {
+      alert("No address");
     }
   }, [address]);
 
